c8322b69542bcb8cb07e6becd09bc2733bc62f1a
Magdalena Sielewicz <magdalena.sielewicz@e-point.pl>
2016-01-19 11:16:29 +0100

    tests

---
+++ src/test/java/tests/google/GoogleTest.java
@@ -0,0 +1,91 @@
+package tests.google;
+
+import google.content.pages.MainPage;
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+/**
+ * Nadklasa dla testów google
+ * Created by Mag.
+ */
+public abstract class GoogleTest {
+
+    private final static Logger logger = Logger.getLogger(GoogleTest.class);
+    private WebDriver driver;
+
+    /**
+     * Operacje wykonywane przed uruchomieniem testów z danej klasy
+     * - Otwarcie przegl¹darki
+     */
+    @BeforeClass
+    public void beforeClass() {
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+    }
+
+    /**
+     * Operacje wykonywane po zakoñczeniu testów z danej klasy
+     * - Zamkniêcie przegl¹darki
+     */
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+
+    /**
+     * Operacje wykonywane po metodzie testowej
+     * - Wykonanie zdjêcia, gdy test nie przeszed³
+     *
+     * @param result
+     */
+    @AfterMethod
+    public void afterMethod(ITestResult result) {
+        Reporter.setCurrentTestResult(result);
+        String methodName = result.getName();
+        if (!result.isSuccess()) {
+            Calendar calendar = Calendar.getInstance();
+            SimpleDateFormat formater = new SimpleDateFormat("dd_MM_yyyy_hh_mm_ss");
+
+            File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            try {
+                String path = "screenshots";
+                File file = new File(path + "/" + methodName + "_" + formater.format(calendar.getTime()) + ".png");
+                FileUtils.copyFile(scrFile, file);
+                System.setProperty("org.uncommons.reportng.escape-output", "false");
+                Reporter.setEscapeHtml(false);
+                Reporter.log("<a " +
+                        "href='" + file.getAbsolutePath() + "'> " +
+                        "<img src=\"./../../../../../" + path + "/" + file.getName() + "\" height=\"50%\" width=\"50%\"/>" +
+                        " </a>");
+                Reporter.setEscapeHtml(false);
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+
+        Reporter.setCurrentTestResult(null);
+    }
+
+    /**
+     * Przejœcie do g³. stony Google
+     *
+     * @return
+     */
+    protected MainPage goToGoogle() {
+        return MainPage.open(driver);
+    }
+}
---
+++ src/test/java/tests/google/search/SimpleSearchTest.java
@@ -0,0 +1,60 @@
+package tests.google.search;
+
+import google.content.components.results.WebSearchResultsList.WebResultRow;
+import google.content.pages.MainPage;
+import google.content.pages.external.ExternalPage;
+import google.content.pages.search.WebSearchResultsPage;
+import org.apache.log4j.Logger;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import tests.google.GoogleTest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.testng.Assert.fail;
+
+/**
+ * Test wyszukiwania w google
+ * Created by Mag.
+ */
+public class SimpleSearchTest extends GoogleTest {
+
+    private final static Logger logger = Logger.getLogger(SimpleSearchTest.class);
+
+    @DataProvider(name = "searchText")
+    protected Object[][] getDataSearchText() {
+        return new Object[][]{
+                {"selenium", "Selenium - Web Browser Automation"},
+        };
+    }
+
+    /**
+     * Test prostego wyszukiwania w Google
+     *
+     * @param searchText        - tekst do wyszukania
+     * @param expectedPageTitle - oczekiwany Tytu? znalezionej strony
+     */
+    @Test(dataProvider = "searchText")
+    public void testSearchWeb(String searchText, String expectedPageTitle) {
+        //otwarcie strony google
+        MainPage mainPage = goToGoogle();
+        //wyszukanie zadanego tesktu w wyszukiwarce google
+        WebSearchResultsPage resultsPage = mainPage.search(searchText);
+        //spr. czy s? jakie? wyniki wyszukiwania
+        assertThat("Brak tabeli wynik?w wyszukiwania", resultsPage.isTableResultsPresent(), equalTo(true));
+        //otwarcie pierwszej z znalezionych stron
+        WebResultRow row = resultsPage.getResultsList().getFirstRow();
+        ExternalPage externalPage = row.open();
+        //spr. czy to otwarto zamierzon? stron? (po jej tytule)
+        assertThat("Tytu? strony", externalPage.getTitle(), equalTo(expectedPageTitle));
+    }
+
+    /**
+     * Test, kt?ry nie przechodzi zawsze
+     */
+    @Test
+    public void testFail() {
+        MainPage mainPage = goToGoogle();
+        fail("OK, test mia? nie przej??");
+    }
+}
